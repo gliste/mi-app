1a6033f8e78ecd883b6f087bdbe9c3e35fecbd04
unknown <gabrielaliste@hotmail.com>
2023-11-24 16:10:00 -0300

    mi-app probando

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import User from './components/User/User';
 import './App.css';
 
 function App() {
+  const a = 10;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <h1>Hola Mundo: {a} </h1>
+      <User Nombre = "Gabriela"/>
     </div>
   );
 }
